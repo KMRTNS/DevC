7cc13a94469e58f4b2e2d86466bb44b171502aed
Igor <igormartins.mcz@gmail.com>
2023-02-11 19:02:56 -0300

    C
    Aula 1.2 - Aprenda a DEBUGAR em Linguagem C. (Debug via Dev C++)

---
+++ C1-2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	printf("\nMensagem 1.");
+	printf("\nMensagem 2.");
+	printf("\nMensagem 3.");
+	printf("\nMensagem 4.");
+	printf("\nMensagem 5.");
+	printf("\nMensagem 6.\n\n");
+	system("pause");
+	return 0;
+}
